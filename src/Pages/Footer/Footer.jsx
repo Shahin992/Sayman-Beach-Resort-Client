d3fc57b51bdbd2e8dd91e64c6f0d91a10f95d126
Shahin992 <shahinkhan5979@gmail.com>
2023-11-06 01:21:26 +0600

    commit with Footer section

---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer p-10 bg-[#2a2b33] text-white ">
+                <div className="flex flex-col justify-center items-center">
+                <img className="h-14 w-14 " src="https://i.ibb.co/fCPFdn9/sayeman-beach-resort-logo-final.png" alt="" />
+    <p className="text-3xl font-bold">Sayeman Beach Resort</p>
+                </div>
+  <nav>
+    <header className="footer-title">Services</header> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <form>
+    <header className="footer-title">Newsletter</header> 
+    <fieldset className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label> 
+      <div className="relative">
+        <input type="email" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
+        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+            
+        </div>
+    );
+};
+
+export default Footer;
