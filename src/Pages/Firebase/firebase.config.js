cd036665304f1362634476353e69f153de164524
Shahin992 <shahinkhan5979@gmail.com>
2023-11-07 03:11:20 +0600

    commit with log in page

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -1,1 +1,14 @@
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.envVITE_apiKey,
+  authDomain: import.meta.envVITE_authDomain,
+  projectId: import.meta.envVITE_projectId,
+  storageBucket: import.meta.envVITE_storageBucket,
+  messagingSenderId: import.meta.envVITE_messagingSenderId,
+  appId: import.meta.envVITE_appId
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
